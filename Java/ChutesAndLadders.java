bb439c1ca6745bb483211a242ee120d69ed8a64c
J.D. Sandifer <jdsandifer@users.noreply.github.com>
2017-05-02 22:25:02 -0700

    Create ChutesAndLadders.java

---
+++ Java/ChutesAndLadders.java
@@ -0,0 +1,3 @@
+// Solving for minimum rolls to complete the Chutes and Ladders game.
+// This problem is from HackerRank.com, uses a 10x10 board, and refers
+// to the game using its original name of Snakes and Ladders.
