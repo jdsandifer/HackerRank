3ce25f8657ad96d16d2d859d90d80dfa5e51b5b2
J.D. Sandifer <jdsandifer@users.noreply.github.com>
2017-05-04 19:52:46 -0700

    Update ChutesAndLadders.java

--- Java/ChutesAndLadders.java
+++ Java/ChutesAndLadders.java
@@ -4,3 +4,22 @@
 
 // I'm practicing whiteboarding so I created this file to show I've finished
 // the whiteboarding, but I don't have time to transfer the code right now...
+
+import java.util.HashMap;
+import java.util.Map;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        // Input reading code to come
+        
+        System.out.println(recursiveMinimumRolls());
+    }
+    
+    public static int recursiveMinimumRolls() {
+        return 1;
+    }
+}
