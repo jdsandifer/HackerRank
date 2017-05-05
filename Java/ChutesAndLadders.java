5e1b14947c1f3cee45ffa228f1d94a1192df7a29
J.D. Sandifer <jdsandifer@users.noreply.github.com>
2017-05-05 15:42:17 -0700

    Add code from whiteboard solution

--- Java/ChutesAndLadders.java
+++ Java/ChutesAndLadders.java
@@ -2,11 +2,15 @@
 // This problem is from HackerRank.com, uses a 10x10 board, and refers
 // to the game using its original name of Snakes and Ladders.
 
-// I'm practicing whiteboarding so I created this file to show I've finished
-// the whiteboarding, but I don't have time to transfer the code right now...
+// I started solving this problem through whiteboarding and am just
+// recording my work so far. After I transfer it to HackeRank and test
+// it, I'll post the working version here.
 
+import java.lang.Math;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import java.util.Scanner;
 
@@ -16,10 +20,34 @@ public class Solution {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         // Input reading code to come
         
-        System.out.println(recursiveMinimumRolls());
+        System.out.println(recursiveMinimumRolls(5, new HashMap(), new HashMap(), 1, new HashSet());
     }
     
-    public static int recursiveMinimumRolls() {
-        return 1;
+    public static int recursiveMinimumRolls(
+            int size, 
+            Map<Integer, Integer> chutes, 
+            Map<Integer, Integer> ladders, 
+            int currentSpace, 
+            Set<Integer> spacesVisited) {
+        Set spacesVisitedNow = new HashSet(spacesVisited.toList());
+        spacesVisitedNow.put(space);
+        if (space == size * size)
+            return 0;
+        else if (chutes.containsKey(space) && !spacesVisited.contains(chutes.get(space))
+            return recursiveMinimumRolls(size, chutes, ladders, chutes.get(space), spacesVisitedNow);
+        else if (chutes.containsKey(space) && spacesVisited.contains(chutes.get(space))
+            return Integer.MAX;
+        else {
+            int minRolls = Integer.MAX;
+            int currentRolls = Integer.MAX;
+            if (ladders.containsKey(space))
+                minRolls = recursiveMinimumRolls(size, chutes, ladders, ladders.get(space), spacesVisitedNow);
+            int possibleRolls = Math.min(6, (size * size) - space);
+            for (int i = 1; i <= possibleRolls; i++) {
+                currentRolls = 1 + recursiveMinimumRoll(size, chutes, ladders, space + i, spacesVisitedNow);
+                minRolls = Math.min(minRolls, currentRolls);
+            }
+            return minRolls;
+        }
     }
 }
