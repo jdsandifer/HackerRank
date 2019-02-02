3ffdb09629e4d58532341d0933dfe5a08288208a
J.D. Sandifer <sandifer.jd@gmail.com>
2019-02-01 20:52:59 -0800

    Delete functional-easy.scala

--- functional-easy.scala
+++
@@ -1,7 +0,0 @@
-/** Return a list like the given list filtered to only include values less than a specified value. */
-def chooseLessThan(delim: Int, arr: List[Int]): List[Int] = arr.filter(_ < delim)
-
-
-/** Return a new list with elements at odd positions removed. (First index is 1.) */
-def f(arr: List[Int]): List[Int] = 
-    arr.zipWithIndex.filterNot((t:Tuple2[Int, Int]) => (t._2 % 2 == 0)).unzip._1
