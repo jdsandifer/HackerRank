948db95330ae448075ebd2fd9edfd3b888d89051
J.D. Sandifer <sandifer.jd@gmail.com>
2019-02-02 20:45:27 -0800

    Add problem and solution

--- Scala/functional-easy.scala
+++ Scala/functional-easy.scala
@@ -1,7 +1,10 @@
 /** Return a list like the given list filtered to only include values less than a specified value. */
-def chooseLessThan(delim: Int, arr: List[Int]): List[Int] = arr.filter(_ < delim)
+def allLessThan(delim: Int, arr: List[Int]): List[Int] = arr.filter(_ < delim)
 
 
 /** Return a new list with elements at odd positions removed. (First index is 1.) */
-def withoutOddElements(arr: List[Int]): List[Int] = 
+def oddIndexesRemoved(arr: List[Int]): List[Int] = 
     arr.zipWithIndex.filterNot((t:Tuple2[Int, Int]) => (t._2 % 2 == 0)).unzip._1
+
+/** Return the sum of odd elements in a list. */
+def sumOfOddElements(numbers: List[Int]):Int = numbers.filter(_ % 2 != 0).foldLeft(0)(_ + _)
